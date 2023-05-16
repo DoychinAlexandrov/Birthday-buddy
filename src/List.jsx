049e36ea5cdcfb67d5afb82f0a20819a70d49dbf
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2023-05-16 17:40:11 +0300

    commit

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Person from "./Person";
+
+const List = ({ people }) => {
+    return (
+        <section>
+            {people.map((person) => {
+                return <Person key={person.key} {...person} />;
+            })}
+        </section>
+    );
+};
+
+export default List;
